a0daa64b5f902682301bef82cc55ec041f03ec72
Beatriz López Moreno <beatlm@gmail.com>
2025-10-05 17:38:29 +0200

    fix config

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true
+    },
+    server: {
+      port: 3000
+    },
+    define: {
+      'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
+      'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY)
+    }
+  };
+});
